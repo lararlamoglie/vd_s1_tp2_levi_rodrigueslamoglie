e49db1d9a9d7f6ddfced46a761a8a682c8e76b1d
Lara <laruu147@gmail.com>
2023-03-18 15:47:20 -0300

    cambios

--- ejemplo2/script.js
+++ ejemplo2/script.js
@@ -1,39 +1,54 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
+  data = data.filter(d => ['EE.UU.', 'U.S.S.R/Rusia', 'Japon', 'Italia', 'Reino Unido', 'Francia', 'Canada', 'Alemania'].includes(d.nacionalidad));
   console.log(data) //ver en pantalla
   // Guardamos el svg generado en la variable chart
   let chart = Plot.plot({ //genera una visualizacion, guarda el gráfico en la variable chart
 
     marks: [ 
-      Plot.text(['Evaluación horas de misión por nacionalidad'], {frameAnchor:"top", fontSize: 18,},),//marcas del plot
-      Plot.barY(data, { //elijo la marca punto para representar cada identidad
-        x: 'nacionalidad', 
-        y: 'eva_mision_hs',
-        fill: 'nacionalidad',
-        stroke: 'nacionalidad',
-        sort: 'nacionalidad',
-      }),
+      Plot.text(['Evaluación horas de misión por nacionalidad'], {
+        frameAnchor:"top", 
+        fill: "black",
+        fontSize: 18,},),//marcas del plot
+      Plot.barY(data, Plot.groupX({y: "sum"}, {x:"nacionalidad", y: "eva_mision_hs", fill: d => d.nacionalidad === "EE.UU." ? "#000073" : (d.nacionalidad === "U.S.S.R/Rusia" ? "#990000" : "#D1C9C8"),})),
+      Plot.text(data, {
+        x: "nacionalidad",
+        y: "sum",
+        text: "sum",
+        fontWeight: "bold",
+        fill: "black",
+        filter: d => d.nacionalidad === "EE.UU.", x: "nacionalidad", y: "eva_mision_hs", text: d => (`${d.sum}`), dy: -300}),
+      Plot.text(data, {
+        x: "nacionalidad",
+        y: "sum",
+        text: "sum",
+        fontWeight: "bold",
+        fill: "black",
+        filter: d => d.nacionalidad === "U.S.S.R/Rusia", x: "nacionalidad", y: "eva_mision_hs", text: d => (`${d.sum}`), dy: -140})
     ],
     x: {
-      domain: d3.sort(data, (a, b) => d3.descending(a.eva_mision_hs, b.eva_mision_hs)).map(d => d.nacionalidad),
+      domain: d3.sort(data, (a, b) => d3.descending(a.sum, b.sum)).map(d => d.nacionalidad),
       label: 'Nacionalidad',
-      grid:true,
       labelOffset: 40,
     },
     y: {
-      grid: true,
       labelOffset: 155,
-      label: "Evaluación de misión en hrs" 
+      label: "Evaluación de misión en hrs",
+      grid: true,
+      domain: [0,700]
     },
     width: 1300,
-    height: 300,
+    height: 400,
     marginLeft: 160,
     marginRight: 200,
-    marginTop:10,
-    marginBottom: 100,
+    marginTop:50,
+    marginBottom: 50,
     line: true,
     nice: true,
     zero: true,
-    grid: true,
+    insetTop: 0,
+    style:{
+      color: "grey",
+    }
   })
   // Agregamos chart al div#chart de index.html
   d3.select('#chart').append(() => chart) //agregamos el grafico a index.html
