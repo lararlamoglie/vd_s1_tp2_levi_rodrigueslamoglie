447aa4aa55945648324fa83293f15445a48f3c33
Lara <laruu147@gmail.com>
2023-03-18 13:58:56 -0300

    update3

--- ejemplo1/script.js
+++ ejemplo1/script.js
@@ -5,41 +5,42 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
     
     y: {
       domain: d3.sort(data, (a, b) => d3.descending(a.mision_hs, b.mision_hs)).map(d => d.ocupacion),
-      labelOffset: 200,
-      grid: true,
+      labelOffset: 180,
+      //grid: true,
       label: 'Ocupación',
       fontSize: 100,
     },
-    x: {
-      grid: true,
-      labelOffset:40,
-      label: 'Misión en horas'
-
-    },
     marks: [ //marcas del plot
       Plot.text(['Horas de misión por ocupación'], {frameAnchor: "top", fontSize: 18,},),
-      Plot.barX(data, { //elijo la marca punto para representar cada identidad
-        x: 'mision_hs', y: 'ocupacion', fill: 'ocupacion', stroke: 'ocupacion'
-        
-      }),
+      Plot.axisX({anchor: "bottom", 
+      //tickFormat: "", 
+      labelOffset: 17,
+      tickSize: 5,}),
+      Plot.barX(data, Plot.groupY({x: "sum"}, {x: "mision_hs", y: "ocupacion", fill:'ocupacion', sort: 'sum',})),
+      Plot.ruleX[0],
+      Plot.gridX({insetTop:80,}),
     ],
-       
+    
+    x:{domain:[0,300000], grid:true, tickFormat:"", type: "linear", grid: true, labelOffset:40, label: 'Misión en horas',
+    },
     style:{
       fontFamily: 'serif',
-      fontSize: 14,
+      fontSize: 12,
       //background: 'hsl(0,100%,50%)'
       color: 'black',
       padding: '100px',
     },
     width: 900,
-    height: 500,
-    marginLeft: 300,
+    height: 450,
+    marginLeft: 200,
     marginBottom: 200,
-    marginTop: 20,
-    marginRight: 70,
+    marginTop: 10,
+    marginRight: 130,
+    insetTop: 25,
     
     color:{
-      scheme: 'orrd'
+      type: "categorical",
+      scheme: 'blues',
     }
   })
   // Agregamos chart al div#chart de index.html
--- ejemplo1c/script.js
+++ ejemplo1c/script.js
@@ -3,20 +3,21 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
   // Guardamos el svg generado en la variable chart
   let chart = Plot.plot({ //genera una visualizacion, guarda el gráfico en la variable chart
     marks: [
-      Plot.text(['Misión en horas de acuerdo al género'], {frameAnchor:"top", fontSize: 18,},),//marcas del plot //marcas del plot
+      Plot.text(['Horas de misión en función del género'], {frameAnchor:"top", fontSize: 16,}, ),//marcas del plot //marcas del plot
       Plot.dot(data, { //elijo la marca punto para representar cada identidad
         x: 'mision_hs', y: 'genero', fill: 'genero', stroke: 'genero',
       }),
     ],
     y: {
-      grid:true,
-      domain: d3.sort(data, (a, b) => d3.descending(a.mision_hs, b.mision_hs)).map(d => d.genero),
-      label: 'Género',
+      //grid:true,
+      //domain: d3.sort(data, (a, b) => d3.descending(a.mision_hs, b.mision_hs)).map(d => d.genero),
+      label: '',
       labelOffset: 110,
     },
     x: {
       grid: true,
       label: 'Misión en horas',
+      labelOffset: 18,
 
     },
     color:{
@@ -24,17 +25,18 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
     },
     style:{
       fontFamily: 'sans-serif',
-      fontSize: 13,
+      fontSize: 12,
       background: 'hsl(4, 73%, 89%)',
       color: 'black',
       padding: '100px',
     },
-    height: 300,
+    height: 150,
     width: 900,
-    marginLeft: 140,
-    marginRight: 50,
-    marginBottom: 50,
-    marginTop: 10,
+    marginLeft: 100,
+    marginRight: 110,
+    marginBottom:40,
+    marginTop: 0,
+    insetTop: 10,
 
   })
   // Agregamos chart al div#chart de index.html
